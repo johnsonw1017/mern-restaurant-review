2e2f322dfb1a96e73d09a9bbe7f0827d0a1d3446
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-06-19 13:16:50 +1000

    feat: configured and started backend development

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+import restaurants from "./api/restaurants.route.js"
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+//specify routes
+app.use("/api/v1/restaurants", restaurants)
+
+//if route does not exist - *
+app.use("*", (request, response) => response.status(404).json({error: "not found"}));
+
+export default app
